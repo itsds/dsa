ca50663a4c7f84cb0fcf6823e3566216c61fb0e2
Durga Shanker <itisds.ai@gmail.com>
2026-08-08 18:15:05 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (31.78%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int index=-1;
+        int len = nums.length;
+        int low=0;
+        int high=len-1;
+        int mid=(low+high)/2;
+
+        while(low<=high){
+            if(nums[mid]==target)
+                return mid;
+
+            else if(target < nums[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            mid=(low+high)/2;
+        }
+        //if(nums[low]==target)
+          //  return low;
+
+        return -1;
+        
+    }
+}
